9bd043c4516214875637c1325b045e8e8458a903
Emma Doyle <emma.ann.doyle@gmail.com>
2025-10-02 20:17:09 -0400

    master:  sync

--- src/orchidarium/main.py
+++ src/orchidarium/main.py
@@ -1,5 +1,6 @@
-#! /usr/bin/env python
-# Read temperature and humidity data from USB sensor in my Orchidarium.
+"""
+Read temperature and humidity data from USB sensor in my Orchidarium.
+"""
 
 
 from __future__ import annotations
@@ -12,10 +13,10 @@ from functools import partial
 from time import sleep
 from usb.core import find, USBTimeoutError, USBError
 from usb.util import claim_interface, release_interface
-from array import array
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from array import array
     from typing import (
         Dict,
         Callable,
@@ -39,18 +40,18 @@ try:
     int(env['USB_PRODUCT_ID'], base=16)
 except TypeError as e:
     log.error(e)
-    
+
 
 def communicate(f: Callable, delay: int =1) -> array:
     """
     Retry api calls to the USB device until they go through.
 
     Args:
-        f (Callable): 
-        delay (int): 
+        f (Callable):
+        delay (int):
 
     Returns:
-        array: 
+        array:
     """
     while True:
         try:
@@ -63,10 +64,10 @@ def communicate(f: Callable, delay: int =1) -> array:
                 log.warning(f'Resource busy, retrying')
                 sleep(delay)
                 continue
-            
+
             log.error(f'{e}')
-            
-            
+
+
 class InterfaceClaim:
     """
     Wrap setup and teardown while connecting to a USB interface in a context manager.
@@ -77,7 +78,7 @@ class InterfaceClaim:
         self.detach = detach
         # Indicate that a detachment of the kernel driver took place.
         self._detached: bool = False
-        
+
     def __enter__(self) -> InterfaceClaim:
         if self.detach:
             # Detaching kernel driver if necessary (e.g., for direct communication)
@@ -85,12 +86,12 @@ class InterfaceClaim:
                 log.warning(f'Detaching kernel driver')
                 communicate(
                     partial(
-                        self.device.detach_kernel_driver, 
+                        self.device.detach_kernel_driver,
                         self.interface
                     )
                 )
                 self._detached = True
-        
+
         communicate(
             partial(
                 claim_interface,
@@ -98,7 +99,9 @@ class InterfaceClaim:
                 self.interface
             )
         )
-        
+
+        return self
+
     def __exit__(self, *args: Any) -> None:
         communicate(
             partial(
@@ -107,15 +110,58 @@ class InterfaceClaim:
                 self.interface
             )
         )
-        
+
         if self.detach and self._detached:
             # Detaching kernel driver if necessary (e.g., for direct communication)
             communicate(
                 partial(
-                    self.device.attach_kernel_driver, 
+                    self.device.attach_kernel_driver,
                     self.interface
                 )
             )
+
+
+def read(endpoint: Any, device: Any) -> bytes:
+    """
+
+
+    Args:
+        endpoint (Any): _description_
+        device (Any): _description_
+
+    Returns:
+        bytes: _description_
+    """
+    return communicate(
+        partial(
+            device.read,
+            endpoint.bEndpointAddress,
+            endpoint.wMaxPacketSize,
+
+        )
+    ).tobytes()
+
+
+def write(endpoint: Any, device: Any, data: bytes) -> bytes:
+    """
+
+
+    Args:
+        endpoint (Any): _description_
+        device (Any): _description_
+        data (bytes):
+    """
+    if len(data) >= endpoint.wMaxPacketSize:
+        log.error(f'Cannot write data of size {len(data)} bytes to bus with max packet size {endpoint.wMaxPacketSize}')
+        return None
+
+    return communicate(
+        partial(
+            device.write,
+            endpoint.bEndpointAddress,
+            data + b'\x00' * (endpoint.wMaxPacketSize - len(data))
+        )
+    )
 
 
 def main() -> int:
@@ -129,11 +175,11 @@ def main() -> int:
             base=16
         )
     )
-    
+
     if device is None:
         log.error(f'USB device with idVendor "{env["USB_VENDOR_ID"]}" and idProduct "{env["USB_PRODUCT_ID"]}" not found, exiting.')
         return 1
-        
+
     with InterfaceClaim(device, detach=True):
         communicate(
             partial(
@@ -145,22 +191,78 @@ def main() -> int:
                 device.set_configuration
             )
         )
-        
+
+        # device_path = f'/dev/bus/usb/{device.bus:03d}/{device.address:03d}'
+        # sht = SHT20(device_path, resolution=SHT20.TEMP_RES_14bit)
+        # temp = sht.read_temp()
+        # humid = sht.read_humid()
+        # print("Temperature (°C): " + str(temp))
+        # print("Humidity (%RH): " + str(humid))
+
         endpoint = device[0][(0,0)][0]
-        
-        for i in range(30):
-            bytes = communicate(
-                partial(
-                    device.read,
-                    endpoint.bEndpointAddress,
-                    endpoint.wMaxPacketSize,
-                    1000
-                )
-            )
-            print(bytes.tobytes())
+
+        # https://sensirion.com/media/documents/CCDE1377/635000A2/Sensirion_Datasheet_Humidity_Sensor_SHT20.pdf
+        TRIG_TEMP_HOLD = b'\0xE3'
+        TRIG_HUMID_HOLD = b'\0xE5'
+        TRIG_TEMP_NOHOLD = b'\0xF3'
+        TRIG_HUMID_NOHOLD = b'\0xF5'
+        WRITE_USER_REG = b'\0xE6'
+        READ_USER_REG = b'\0xE7'
+        SOFT_RESET = b'\0x00'
+
+        TEMP_RES_14bit = b'\0x00'
+        TEMP_RES_13bit = b'\0x80'
+        TEMP_RES_12bit = b'\0x01'
+        TEMP_RES_11bit = b'\0x81'
+        END_OF_BATTERY = b'\0x40'
+        ENABLE_HEATER = b'\0x04'
+        DISABLE_OTP_RELOAD = b'\0x02'
+
+        user_data = write(
+            endpoint,
+            device,
+            READ_USER_REG
+        )
+
+        print(user_data)
+        user_data &= 0x38
+        print(user_data)
+        user_data = bin(user_data)
+        user_data |= TEMP_RES_14bit + DISABLE_OTP_RELOAD
+
+        write(
+            endpoint,
+            device,
+            WRITE_USER_REG + user_data
+        )
+
+        write(
+            endpoint,
+            device,
+            SOFT_RESET
+        )
+
+        write(
+            endpoint,
+            device,
+            TRIG_TEMP_NOHOLD
+        )
+
+        write(
+            endpoint,
+            device,
+            TRIG_HUMID_NOHOLD
+        )
 
 
-if __name__ == '__main__': 
+
+def cli() -> None:
+    sys.exit(
+        main()
+    )
+
+
+if __name__ == '__main__':
     sys.exit(
         main()
     )
