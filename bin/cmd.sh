7d80eb5dadb7c083e5d19414e0154b32d58a1e47
Emma Doyle <emma.ann.doyle@gmail.com>
2025-11-05 05:56:42 -0500

    master: sync

--- bin/cmd.sh
+++ bin/cmd.sh
@@ -5,15 +5,15 @@
 set -eo pipefail
 
 
-if [ -z "$USB_VENDOR_ID" ]; then
-    printf "ERROR: USB_VENDOR_ID is not defined. Please provide a valid USB vendor ID in hexadecimal (from lsusb -v output).\\n" >&2
-    exit 1
-fi
+# if [ -z "$USB_VENDOR_ID" ]; then
+#     printf "ERROR: USB_VENDOR_ID is not defined. Please provide a valid USB vendor ID in hexadecimal (from lsusb -v output).\\n" >&2
+#     exit 1
+# fi
 
-if [ -z "$USB_PRODUCT_ID" ]; then
-    printf "ERROR: USB_PRODUCT_ID is not defined. Please provide a valid USB product ID in hexadecimal (from lsusb -v output).\\n" >&2
-    exit 1
-fi
+# if [ -z "$USB_PRODUCT_ID" ]; then
+#     printf "ERROR: USB_PRODUCT_ID is not defined. Please provide a valid USB product ID in hexadecimal (from lsusb -v output).\\n" >&2
+#     exit 1
+# fi
 
 # Start the service.
 if [ -n "$(poetry env info -p)" ]; then
